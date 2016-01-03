ca131751211b5110265f2c6b7887ae4f10fa213c
Viacheslav Kisel <primazp@icloud.com>
2016-01-03 11:07:01 +0300

    Setup tests

---
+++ test/spec/reducer.spec.js
@@ -0,0 +1,14 @@
+import expect from 'expect'
+import reducer from '../../src/reducer'
+import {createStore} from 'redux'
+
+describe('export', () => {
+  it('combines reducers', () => {
+    const store = createStore(reducer)
+    expect(store.getState()).toEqual({
+      channels: [],
+      users: [],
+      messages: []
+    })
+  })
+})
